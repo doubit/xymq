f30a0a5709476b418dbcd61ddf0279847b14f8cd
xiaoyao <xiaoyao@mogujie.com>
2014-10-30 19:18:15 +0800

    add nameserver

--- apps/nameserver/main.go
+++ apps/nameserver/main.go
@@ -1,11 +1,38 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"github.com/doubit/xymq/nameserver"
+	//"github.com/doubit/xymq/nameserver"
+	"github.com/doubit/xymq/util"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+var (
+	flagSet = flag.NewFlagSet("nameserver", flag.ExitOnError)
+
+	showVersion      = flagSet.Bool("version", false, "print version")
+	verbose          = flagSet.Bool("verbose", false, "enable verbose logging")
+	tcpAddress       = flagSet.String("tcp-address", "0.0.0.0:9812", "<addr>:<port>")
+	broadcastAddress = flagSet.String("broadcast-address", "", "address of this nameserver node, (default to the OS hostname)")
+
+	inactiveProducerTimeout = flagSet.Duration("inactive-producer-timeout", 300*time.Second, "duration of time a broker will remain in the active list since its last ping")
+	tombstoneLifetime       = flagSet.Duration("tombstone-lifetime", 45*time.Second, "duration of time a broker will remain tombstoned if registration remains")
 )
 
 func main() {
-	nameserver.DoNothing()
 	fmt.Println("nameserver....")
+	flagSet.Parse(os.Args[1:])
+
+	if *showVersion {
+		fmt.Println(util.Version("nameserver"))
+		return
+	}
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+
 }
--- nameserver/nameserver.go
+++ nameserver/nameserver.go
@@ -1,5 +1,64 @@
 package nameserver
 
-func DoNothing() bool {
-	return true
+import (
+	"fmt"
+	"github.com/doubit/xymq/util"
+	"net"
+	"os"
+)
+
+type XYMQNameServer struct {
+	opts        *xymqNameServerOptions
+	tcpAddr     *net.TCPAddr
+	tcpListener net.Listener
+	waitGroup   util.WaitGroupWrapper
+	DB          *RegistrationDB
+}
+
+func NewXYMQNameServer(opts *xymqNameServerOptions) *NSQLookupd {
+	n := &XYMQNameServer{
+		opts: opts,
+		DB:   NewRegistrationDB(),
+	}
+
+	tcpAddr, err := net.ResolveTCPAddr("tcp", opts.TCPAddress)
+	if err != nil {
+		n.logf("FATAL: failed to resolve TCP address (%s) - %s", opts.TCPAddress, err)
+		os.Exit(1)
+	}
+	n.tcpAddr = tcpAddr
+
+	n.logf(util.Version("nameserver"))
+
+	return n
+}
+
+func (n XYMQNameServer) logf(f string, args ...interface{}) bool {
+	if n.opts.Logger == nil {
+		return
+	}
+	n.opts.Logger.Output(2, fmt.Sprintf(f, args...))
+}
+
+func (s *XYMQNameServer) Main() {
+	ctx := &Context{s}
+
+	tcpListener, err := net.Listen("tcp", l.tcpAddr.String())
+	if err != nil {
+		s.logf("FATAL: listen (%s) failed - %s", s.tcpAddr, err)
+		os.Exit(1)
+	}
+	s.tcpListener = tcpListener
+	tcpServer := &tcpServer{ctx: ctx}
+	s.waitGroup.Wrap(func() {
+		util.TCPServer(tcpListener, tcpServer, s.opts.Logger)
+	})
+}
+
+func (l *XYMQNameServer) Exit() {
+	if l.tcpListener != nil {
+		l.tcpListener.Close()
+	}
+
+	l.waitGroup.Wait()
 }
